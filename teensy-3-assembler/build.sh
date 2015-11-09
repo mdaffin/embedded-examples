52befd9cc76e57cac20aa8b91bee2be3a1d1ede9
Michael Daffin <james1479@gmail.com>
2015-11-09 22:09:52 +0000

    Renames linker.ld to layout.ld
    I have seen more sources call it this.

--- teensy-3-assembler/build.sh
+++ teensy-3-assembler/build.sh
@@ -6,7 +6,7 @@ OUTDIR=out
 mkdir -p "${OUTDIR}"
 rm -f "${OUTDIR}"/*
 arm-none-eabi-as -g -mcpu=cortex-m4 -mthumb -o "${OUTDIR}"/crt0.o crt0.s
-arm-none-eabi-ld -T linker.ld -o "${OUTDIR}"/crt0.elf "${OUTDIR}"/crt0.o
+arm-none-eabi-ld -T layout.ld -o "${OUTDIR}"/crt0.elf "${OUTDIR}"/crt0.o
 arm-none-eabi-objcopy -O ihex -R .eeprom "${OUTDIR}"/crt0.elf "${OUTDIR}"/crt0.hex
 arm-none-eabi-objcopy -O binary -R .eeprom "${OUTDIR}"/crt0.elf "${OUTDIR}"/crt0.bin
 echo "Reset teensy now"

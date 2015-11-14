84d2dbf5a53fd09ee14a488545051e5d99d4f35b
Michael Daffin <james1479@gmail.com>
2015-11-14 02:07:00 +0000

    Adds globals init

--- teensy-3-c/crt0.c
+++ teensy-3-c/crt0.c
@@ -27,6 +27,8 @@ This text refers to the programmers manual for the MK20DX256VLH7. You can
 obtain it from: https://www.pjrc.com/teensy/K20P64M72SF1RM.pdf
 */
 
+#include <stdlib.h>
+
 #define WDOG_UNLOCK  (*(volatile unsigned short *)0x4005200E) // Watchdog Unlock register
 #define WDOG_STCTRLH (*(volatile unsigned short *)0x40052000) // Watchdog Status and Control Register High
 #define GPIO_CONFIG  (*(volatile unsigned short *)0x40048038)
@@ -34,7 +36,12 @@ obtain it from: https://www.pjrc.com/teensy/K20P64M72SF1RM.pdf
 #define GPIOC_PDDR   (*(volatile unsigned short *)0x400FF094) // GPIOC_PDDR - page 1334,1337
 #define GPIOC_PDOR   (*(volatile unsigned short *)0x400FF080) // GPIOC_PDOR - page 1334,1335
 
-extern unsigned int _estack;
+extern unsigned long _etext;
+extern unsigned long _sdata;
+extern unsigned long _edata;
+extern unsigned long _sbss;
+extern unsigned long _ebss;
+extern unsigned long _estack;
 
 void startup();
 void nim_handler();
@@ -45,7 +52,7 @@ void usage_fault_handler();
 void loop();
 void led_on();
 void led_off();
-void delay();
+void delay(int ms);
 
 __attribute__ ((section(".vectors"), used))
 void (* const _vectors[7])(void) = {
@@ -62,15 +69,20 @@ __attribute__ ((section(".flashconfig"), used))
 const unsigned char flashconfigbytes[16] = {
 	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
 	0xFF, 0xFF, 0xFF, 0xFF, 0xFE, 0xFF, 0xFF, 0xFF
-
 };
 
+__attribute__ ((section(".startup")))
 void startup() {
+	unsigned long *src = &_etext;
+	unsigned long *dest = &_sdata;
+
   WDOG_UNLOCK  = ((unsigned short)0xC520);
   WDOG_UNLOCK  = ((unsigned short)0xD928);
   WDOG_STCTRLH = ((unsigned short)0x01D2);
-  __asm__ volatile ("nop");
-  __asm__ volatile ("nop");
+
+	while (dest < &_edata) *dest++ = *src++;
+	dest = &_sbss;
+	while (dest < &_ebss) *dest++ = 0;
 
   // Enable system clock on all GPIO ports - page 254
   GPIO_CONFIG = ((unsigned short)0x00043F82); // 0b1000011111110000010
@@ -82,12 +94,13 @@ void startup() {
   loop();
 }
 
+int n = 1000; // Used to test if the data section is copied correctly
 void loop() {
   while (1) {
     led_on();
-    delay();
+    delay(n);
     led_off();
-    delay();
+    delay(n);
   }
 }
 
@@ -100,8 +113,8 @@ void led_off() {
   GPIOC_PDOR = ((unsigned short)0x0);
 }
 
-void delay() {
-  for (unsigned int i = 0; i <= 2500000; i++) {;}
+void delay(int ms) {
+  for (unsigned int i = 0; i <= ms * 2500; i++) {;}
 }
 
 void nim_handler() { while (1); }

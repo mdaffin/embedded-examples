216ef0b33a9ec445c99d0c3164b4984c0b722d4a
Michael Daffin <james1479@gmail.com>
2015-12-07 01:41:18 +0000

    Updates build.sh

--- teensy-3-rust/build.sh
+++ teensy-3-rust/build.sh
@@ -3,5 +3,5 @@ git -C rustcore checkout $(rustc --version -v | awk '/commit-hash/ {print $2}')
 
 mkdir libcore-thumbv7em
 rustc -C opt-level=2 -Z no-landing-pads --target thumbv7em-none-eabi -g rustcore/src/libcore/lib.rs --out-dir libcore-thumbv7em
-rustc -C opt-level=2 -Z no-landing-pads --target thumbv7em-none-eabi -g --emit obj -L libcore-thumbv7em -o blink.o blink.rs
-arm-none-eabi-ld -T layout.ld blink.o -o blink.elf
+#rustc --crate-type=bin --crate-name=blink -C ar=arm-none-eabi-ar -C linker=arm-none-eabi-gcc -C opt-level=2 -Z no-landing-pads --target thumbv7em-none-eabi -g --emit dep-info,link -L libcore-thumbv7em -o blink.elf blink.rs --extern core=libcore-thumbv7em/libcore.rlib
+rustc --crate-type=bin --crate-name=blink -C ar=arm-none-eabi-ar -C linker=arm-none-eabi-gcc -Z no-landing-pads --target thumbv7em-none-eabi -g -L libcore-thumbv7em blink.rs --extern core=libcore-thumbv7em/libcore.rlib
